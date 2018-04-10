3853c21d9fd9276592fd7934b1c9af02bed48d97
pedro77pl <piotr@lagoda.pl>
2018-04-11 00:54:23 +0200

    script

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# Stipend-VPS-gui-wallet
+
+One click script to install a [Stipend Wallet](http://stipend.me) on a Vultr.com VPS server running Ubuntu 16.04.  
+Perfect for high uptime staking wallets.  
+Use it on your own risk!
+
+## Installation:  
+
+Download this script and use as Vultr.com setup script. Works fine with 512MB Ubuntu 16.04 single core instance.
+Full installation tutorial available at [Stipend](http://stipend.me).
+
+## Donations:
+
+Any donation is highly appreciated  
+
+**SPD**: SPKA38KfURfCDf3E79fM6YbsJGV2tyLp1E  
+**BTC**: 1LK4bjkYVG9aJAA5WUmV5d5KqVYHbMNiwt  
+**ETH**: 0x4D8456a43bE446645E61cd1cAd740b362563fDA4  
+**LTC**: LYCEQLd41M1i5E3LjeRHGgZrp8AKPA21Lt  
---
+++ setup_wallet.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+
+TMP_FOLDER=$(mktemp -d)
+WALLET_URL="https://github.com/Stipend-Developer/stipend/releases/download/1.0.7/stipend-linux-1.0.7.zip"
+WALLET_ARCH="stipend-linux-1.0.7.zip"
+WALLET_DIR="/root/Desktop"
+WGET="/usr/bin/wget"
+
+function prepare_swap() {
+	if free | awk '/^Swap:/ {exit !$2}'; then
+		printf "\nSwap exists"
+	else
+		dd if=/dev/zero of=/swapfile count=2048 bs=1M
+		chmod 600 /swapfile
+		mkswap /swapfile
+		swapon /swapfile
+		echo "/swapfile none swap sw 0 0" >> /etc/fstab
+	fi
+}
+
+function update_system() {
+	apt-get -y update
+}
+
+function install_packages() {
+apt-get -yq install lubuntu-core
+apt-get -yq install lxterminal
+apt-get -yq install xarchiver
+apt-get -yq install firefox
+apt-get -yq install libdb5.3++
+apt-get -yq install libboost-all-dev
+apt-get -yq install qtbase5-dev
+apt-get -yq install libminiupnpc-dev
+}
+
+function create_xorg() {
+	touch /usr/share/X11/xorg.conf.d/xorg.conf
+	cat > /usr/share/X11/xorg.conf.d/xorg.conf << EOL
+
+	Section "Device"
+		Identifier      "device"
+	EndSection
+
+	Section "Screen"
+		Identifier      "screen"
+		Device          "device"
+		Monitor         "monitor"
+		DefaultDepth    24
+
+		SubSection "Display"
+			Modes       "1280x800" "1280x1024" "1280x960" "1280x768" "1280x720" "1024x768" "800x600"
+		EndSubSection
+
+	EndSection
+
+	Section "Monitor"
+		Identifier      "monitor"
+		HorizSync       0.0 - 100.0
+		VertRefresh     0.0 - 100.0
+	EndSection
+
+	Section "ServerLayout"
+		Identifier      "layout"
+		Screen          "screen"
+	EndSection
+EOL
+}
+
+function install_wallet() {
+	cd $TMP_FOLDER
+	$WGET $WALLET_URL
+	mkdir $WALLET_DIR
+	mkdir $WALLET_DIR
+	unzip $WALLET_ARCH -d $WALLET_DIR
+}
+
+function enable_autologin() {
+touch /etc/lightdm/lightdm.conf
+cat > /etc/lightdm/lightdm.conf << EOL
+[SeatDefaults]
+autologin-user=root
+autologin-user-timeout=0
+user-session=ubuntu
+EOL
+}
+
+
+
+#***************************** main *********************************************************
+
+printf "\nPlease wait for startup script to finish and login window to appear.\nIt can take serveral minutes - be patient!"  > /dev/tty1
+
+printf "\n\nCreating and turning on SWAP" > /dev/tty1
+prepare_swap
+
+printf "\n\nInstalling updates" > /dev/tty1
+update_system
+
+printf "\n\nDownloading and installing required packages\n" > /dev/tty1
+install_packages
+
+printf "\n\nCreate proper xorg.conf for X" > /dev/tty1
+create_xorg
+
+printf "\n\nDownloading and installing SPD wallet" > /dev/tty1
+install_wallet
+enable_autologin
+
+systemctl start lightdm
